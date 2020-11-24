114eefe0fafe3a6bb1c38f9fffd5d33412dd537d
Maurice <mrvolkwyn@gmail.com>
2020-11-24 11:01:57 +0200

    Add axios package and imported in to home using jsonplacehoolder comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,17 @@
     <div class="container m-auto p-5 bg-gray-700">
       <input type="text" placeholder="Enter Search... " class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
     </div>
+    <div v-for="comment in comments" v-bind:key="comment.id">
+      {{comment.name}}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue'
+import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {
@@ -17,8 +22,13 @@ export default {
   },
   data () {
     return {
-      title: 'Maurice Project Landing Page'
+      title: 'Maurice Project Landing Page',
+      comments: []
     }
+  },
+  created () {
+    axios.get('https://jsonplaceholder.typicode.com/comments')
+      .then(response => (this.comments = response.data))
   }
 }
 </script>
