5365ed8bb5dee65218ab538808f81eea5e747a5e
Maurice <mrvolkwyn@gmail.com>
2020-11-24 15:56:08 +0200

    Mobile menu wip

--- src/components/layouts/Nav.vue
+++ src/components/layouts/Nav.vue
@@ -5,7 +5,7 @@
         <div class="relative flex items-center justify-between h-16">
           <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <!-- Mobile menu button-->
-            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false" @click="openMenu">
               <span class="sr-only">Open main menu</span>
               <!-- Icon when menu is closed. -->
               <!--
@@ -42,7 +42,6 @@
           </div>
         </div>
       </div>
-
       <!--
         Mobile menu, toggle classes based on menu state.
 
@@ -61,6 +60,11 @@
 
 <script>
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  methods: {
+    openMenu () {
+      console.log('open')
+    }
+  }
 }
 </script>
